dd6a7a4398c859782c83591d760eb97a8770dd84
Chelsea Urquhart <me@chelseau.com>
2015-12-28 08:36:08 -0800

    Make ConnectionSettings methods virtual

--- include/ConnectionSettings.h
+++ include/ConnectionSettings.h
@@ -15,24 +15,21 @@ class ConnectionSettings : public SmartObject
 {
 public:
     explicit ConnectionSettings(ConnectionSettings *parent = 0);
-    ConnectionSettings *getParent();
+    virtual ConnectionSettings *getParent();
     virtual void setParent(ConnectionSettings *parent);
-    void remove(std::string key);
-    bool contains(std::string key);
-    Variant get(std::string key, bool bubble = true);
-    void set(std::string key, const Variant value);
-    ConnectionType getType();
+    virtual void remove(std::string key);
+    virtual bool contains(std::string key);
+    virtual Variant get(std::string key, bool bubble = true);
+    virtual void set(std::string key, const Variant value);
+    virtual ConnectionType getType();
 
     static std::vector<ConnectionSettings *> load(FileFormat format,
         std::string filename);
-    static std::vector<ConnectionSettings *> loadBinary(std::string filename);
-    static std::vector<ConnectionSettings *> loadJson(std::string filename);
 
     static void save(std::vector<ConnectionSettings *> &settings,
         FileFormat format, std::string filename);
 
-    void operator<<(const VariantMap &value);
-    void operator>>(VariantVector &value);
+    virtual void operator<<(const VariantMap &value);
     ~ConnectionSettings();
 
 private:
