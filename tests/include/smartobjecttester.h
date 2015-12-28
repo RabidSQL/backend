90520767f91a34bdc506f105fc86451862bd24ae
Chelsea Urquhart <me@chelseau.com>
2015-12-28 13:03:09 -0800

    Change default arg in SmartObjectTester to nullptr instead of 0

--- tests/include/smartobjecttester.h
+++ tests/include/smartobjecttester.h
@@ -8,7 +8,7 @@ namespace RabidSQL {
 
 class SmartObjectTester : public SmartObject {
 public:
-    SmartObjectTester(SmartObject *parent = 0);
+    SmartObjectTester(SmartObject *parent = nullptr);
     static bool contains(SmartObjectTester *value);
     static void reset();
     void queueData(int id, const VariantVector &arguments);
